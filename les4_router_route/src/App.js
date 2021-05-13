723082b2c20e56b0dd00ca93f637cf04395eef4a
OleksiiKobiuk <al_kob@ukr.net>
2021-05-13 07:56:54 +0300

    les 4 without posts

--- les4_router_route/src/App.js
+++ les4_router_route/src/App.js
@@ -5,16 +5,28 @@ import {
     Route,
     Link
 } from 'react-router-dom';
+import Home from "./components/home/Home";
+import Users from "./components/users/Users";
+import Posts from "./components/posts/Posts";
 
 export default function App() {
     return (
         <Router>
-        <div>
-            <Link to={'/'}>Home</Link>
-            <Link to={'/foo'}>foo</Link>
-
-
-        </div>
+            <div>
+                <Link to={'/'}>Home</Link>
+                <br/>
+                <Link to={'/users'}>users</Link>
+                <br/>
+                <Link to={'/posts'}>posts</Link>
+                <Switch>
+                    <Route exact path={'/'}>
+                        <Home/>
+                    </Route>
+                    <Route path={'/users'} component={Users}/>
+                    <Route path={'/posts'} component={Posts}/>
+                    {/*/!*<Route path={'/posts'} render={() => <Posts/>}/>*!/ чомусь не було історії?*/}
+                </Switch>
+            </div>
         </Router>
     );
 }
---
+++ les4_router_route/src/components/users/Users.js
@@ -0,0 +1,31 @@
+import {useEffect, useState} from "react";
+import User from "../user/User";
+import UserDetails from "../user-details/UserDetails";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom';
+
+export default function Users(props) {
+    let {match: {url}} = props;
+    let [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(value => value.json())
+            .then(value => {
+                setUsers([...value]);
+            });
+    }, []);
+    return (
+        <div>
+            {
+                users.map(value => <User key={value.id} item = {value} url={url}/>)
+            }
+            <Switch>
+                <Route path={'/users/:id'} component={UserDetails}/>
+            </Switch>
+        </div>
+    );
+}
