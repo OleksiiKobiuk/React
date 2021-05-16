497c3450f81a2664d739756b8c56566b06e35fe4
OleksiiKobiuk <al_kob@ukr.net>
2021-05-16 21:05:25 +0300

    les 4 + Link to pathname, state

--- les4_router_route/src/components/user-details/UserDetails.js
+++ les4_router_route/src/components/user-details/UserDetails.js
@@ -1,17 +1,18 @@
 // import {useParams} from 'react-router-dom';
 import {useEffect, useState} from "react";
 export default function UserDetails(props) {
-    // let params = useParams();
-    // console.log(props);
-    let {match: {params: {id}}} = props;
+    let {match: {params: {id}}, location: {state}} = props;
     let[user,setUser] = useState(null); //передаємо 1 юзера
-    useEffect(() => {
-         fetch('https://jsonplaceholder.typicode.com/users/' + id)
-            .then(value => value.json())
-            .then(value => {
-                setUser({...value});
-            });
-    }, [user])
+    // useEffect(() => {
+    //      fetch('https://jsonplaceholder.typicode.com/users/' + id)
+    //         .then(value => value.json())
+    //         .then(value => {
+    //             setUser({...value});
+    //         });
+    // }, [id])
+     useEffect(() => {
+         setUser(state);
+    }, [id])
     return (
         <div>
             {user && <h3>{user.id} - {user.name}: {user.email}</h3>}
--- les4_router_route/src/components/user/User.js
+++ les4_router_route/src/components/user/User.js
@@ -4,7 +4,7 @@ export default function User({item, url}) {
     return (
         <div>
             {item.name}
-            <Link to={url+'/' + item.id}>   Details</Link>
+            <Link to={{pathname:`${url}/${item.id}`, state: item}}>   Details</Link>
         </div>
     );
 }
