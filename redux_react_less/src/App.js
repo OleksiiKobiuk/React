0db5ee7c26f4292b3d2b78dd5de25ff0ab620205
OleksiiKobiuk <al_kob@ukr.net>
2021-05-27 00:48:38 +0300

    redux_lesson2

--- redux_react_less/src/App.js
+++ redux_react_less/src/App.js
@@ -1,30 +1,52 @@
 import './App.css';
-import React, {useState} from "react";
+import React, {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
+import {
+    START_POSTS_LOADING,
+    STOP_POSTS_LOADING,
+    SET_POSTS
+} from './redux';
 
-
-const Counter = () => {
-  const counterReact = useSelector((state) => state.counter)
-  // const counterReact = useSelector(({counter}) => counter)   аналогічно до запису вище
-  const dispatch = useDispatch();
-return (
-    <>
-      <h1>Counter: {counterReact}</h1>
-      <button onClick={()=> {dispatch({type:'INC'})}}>inc</button>
-      <button onClick={()=> {dispatch({type:'DEC'})}}>dec</button>
-      <button onClick={()=> {dispatch({type:'RESET'})}}>reset</button>
-    </>
-
-)
-}
-export default function App() {
-    let[value, setValue] = useState(0);
+const Posts = () => {
+    const data = useSelector((state) => state);
     const dispatch = useDispatch();
+    console.log(data);
+    const postsFetcher = async () => {
+        try {
+            dispatch({type: START_POSTS_LOADING});
+            const resp = await fetch('https://jsonplaceholder.typicode.com/posts')
+            const data = await resp.json();
+            dispatch({type: SET_POSTS, payload: data});
+        } catch (e) {
+            console.log(e);
+        } finally {
+            dispatch({type: STOP_POSTS_LOADING});
+        }
+    };
+
+    useEffect(() => {
+        postsFetcher();
+    }, []);
+
+    if (data.isPostsLoading) {
+        return <h1>Posts are loading!!!</h1>
+    }
+
     return (
         <div>
-        <Counter/>
-        <input type="number" value={value} onChange={({target: {value}}) => setValue(value)}/>
-        <button onClick={()=> {dispatch({type:'INC_CUSTOM', payload: Number(value)})}}>Inc custom</button>
+            {data.posts.map(({id, title, body}) => (
+                <p key={id}>
+                    {title} -----> <br/> {body}
+                </p>
+            ))}
+        </div>
+    )
+};
+export default function App() {
+
+    return (
+        <div>
+            <Posts/>
         </div>
     );
 }
--- redux_react_less/src/index.js
+++ redux_react_less/src/index.js
@@ -1,51 +1,10 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {createStore} from 'redux';
+import {store} from './redux';
 import {Provider} from "react-redux";
 
-const initialState = {
-    counter: 1,
-    counter2: 0
-}
 
-const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case'INC': {
-            return {
-                ...state, counter: state.counter + 1
-            }
-        }
-        case'DEC': {
-            return {
-                ...state, counter: state.counter - 1
-            }
-        }
-        case'RESET': {
-            return {
-                ...state, counter: 0
-            }
-        }
-        case'INC_CUSTOM': {
-            return {
-                ...state, counter: state.counter + action.payload
-            }
-        }
-        default: return state
-    }
-}
-const store = createStore(reducer);
-// store.subscribe(() => {
-// console.log(store.getState(), '!!!!');
-// });
-// store.dispatch({type: 'INC'});
-// store.dispatch({type: 'INC'});
-// store.dispatch({type: 'INC'});
-// store.dispatch({type: 'INC'});
-// store.dispatch({type: 'DEC'});
-// store.dispatch({type: 'DEC'});
 
 ReactDOM.render(
     <React.StrictMode>
@@ -56,7 +15,4 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
